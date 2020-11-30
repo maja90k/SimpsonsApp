8d1e51c06487c7e478bb6108344c1ef490a6d8a5
Byron Pastrian <bdh-99@hotmail.cl>
2020-11-30 02:44:59 -0300

    ordene codigo y comente ideas

--- Fuentes/app/src/main/java/cl/inacap/simpson/MainActivity.java
+++ Fuentes/app/src/main/java/cl/inacap/simpson/MainActivity.java
@@ -27,24 +27,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.vp = findViewById(R.id.vPager);
-        this.button = findViewById(R.id.soliBtn);
 
-        this.spinner = findViewById(R.id.spinner_per);
+        this.vp   =     findViewById(R.id.vPager);
+        this.button =   findViewById(R.id.soliBtn);
+        this.spinner =  findViewById(R.id.spinner_per);
         ArrayAdapter<CharSequence> opcion = ArrayAdapter.createFromResource(this,R.array.frases, android.R.layout.simple_spinner_item );
         spinner.setAdapter(opcion);
 
+        //solicitarConsejoBtn
         this.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 if (spinner!=null){
+                    //Aqui  despues de haber solicitado el "consejo"
+                    //deberia de mostrar el viewpager con el consejo y del personaje proviniente.
+                    //pero como obtenemos los datos de la api dentro del viewPager?
+
+
 
                 }else{
                     Toast.makeText(getApplicationContext(),"Seleccione un personaje", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+
+
+
 
 
 
