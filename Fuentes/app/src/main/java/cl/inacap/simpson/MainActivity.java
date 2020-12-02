c792fe30523049e9b7176b3dd0ec6c02a0c020a2
Patricio2098 <reyes.patricio.p@gmail.com>
2020-12-02 10:52:49 -0300

    correccion

--- Fuentes/app/src/main/java/cl/inacap/simpson/MainActivity.java
+++ Fuentes/app/src/main/java/cl/inacap/simpson/MainActivity.java
@@ -19,7 +19,6 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-<<<<<<< HEAD
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -31,32 +30,22 @@ import com.google.gson.JsonArray;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-=======
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
-import com.squareup.picasso.Request;
-
->>>>>>> f4739e0f6ebce6bb37bce295fb5e1229a7632a98
+//import com.squareup.picasso.Request;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import cl.inacap.simpson.adapters.PersonajesAdapters;
-<<<<<<< HEAD
+
 import cl.inacap.simpson.dto.Personaje;
-=======
-<<<<<<< HEAD
-import cl.inacap.simpson.dto.Personaje;
-=======
->>>>>>> f4739e0f6ebce6bb37bce295fb5e1229a7632a98
-import cl.inacap.simpson.fragments.PersonajesFragment;
->>>>>>> 264d5fc4d05ed4fb56e62ccf5cadf71859c6c330
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -64,13 +53,6 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private ListView pl;
     private List<Personaje> personajes;
-    private PersonajesAdapters pa;
-    private PersonajesFragment pf;
-
-
-
-    private List<Personaje> personajes = new ArrayList<>();
-    private ListView persList;
     private PersonajesAdapters personajesAdapter;
     private RequestQueue queue;
 
@@ -79,57 +61,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
         this.pl = findViewById(R.id.persoList);
-=======
-<<<<<<< HEAD
-        this.vp = findViewById(R.id.vPager);
         this.button = findViewById(R.id.soliBtn);
         this.spinner = findViewById(R.id.spinner_per);
-
         ArrayAdapter<CharSequence> opcion = ArrayAdapter.createFromResource(this, R.array.frases, android.R.layout.simple_spinner_item);
-=======
-        this.vp   =     findViewById(R.id.vPager);
->>>>>>> f4739e0f6ebce6bb37bce295fb5e1229a7632a98
-        this.button =   findViewById(R.id.soliBtn);
-
-        this.spinner =  findViewById(R.id.spinner_per);
-        ArrayAdapter<CharSequence> opcion = ArrayAdapter.createFromResource(this,R.array.frases, android.R.layout.simple_spinner_item );
->>>>>>> 264d5fc4d05ed4fb56e62ccf5cadf71859c6c330
         spinner.setAdapter(opcion);
-        jsonDownload();
-
+        this.button =   findViewById(R.id.soliBtn);
         this.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-<<<<<<< HEAD
+
                 if (spinner!=null){
                     jsonDownload();
                     //deberia de mostrar el viewpager con el consejo y del personaje proviniente.
-
-=======
-                if (spinner != null) {
                     //Aqui  despues de haber solicitado el "consejo"
-                    //deberia de mostrar el viewpager con el consejo y del personaje proviniente.
-<<<<<<< HEAD
 
 
-                } else {
-                    Toast.makeText(getApplicationContext(), "Seleccione un personaje", Toast.LENGTH_SHORT).show();
-=======
-                    vp.getCurrentItem();
->>>>>>> f4739e0f6ebce6bb37bce295fb5e1229a7632a98
                     //pero como obtenemos los datos de la api dentro del viewPager?
 
                 }else{
                     Toast.makeText(getApplicationContext(),"Seleccione un personaje", Toast.LENGTH_SHORT).show();
->>>>>>> 264d5fc4d05ed4fb56e62ccf5cadf71859c6c330
+
                 }
             }
         });
-
-<<<<<<< HEAD
     }
+
     public void parJSON(View view){
 
     }
@@ -169,42 +126,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-=======
-        public static void consumoApi() {
-            queue = Volley.newRequestQueue(this.getActivity());
-            this.persList = findViewById(R.id.personajes_Lv);
-            this.personajesAdapter = new PersonajesAdapters(this.getActivity(), R.layout.list_personajes, this.personajes);
-            this.persList.setAdapter(this.personajesAdapter);
-            JsonObjectRequest jsonReq = new JsonObjectRequest(Request.Method.GET, "https://thesimpsonsquoteapi.glitch.me/quotes?count=num", null,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                personajes.clear();
-                                Personaje[] array =
-                                        new Gson().fromJson(response.getString("results"), Personaje[].class);
-                                personajes.addAll(Arrays.asList(array));
-                            } catch (Exception ex) {
-                                personajes.clear();
-                                Log.e("PERSONAJES FRAGMENT", "ERROR");
-                            } finally {
-                                personajesAdapter.notifyDataSetChanged();
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    personajes.clear();
-                    Log.e("PERSONAJES FRAGMENT", "ERROR");
-                    personajesAdapter.notifyDataSetChanged();
-                }
-            });
-            queue.add(jsonReq);
-        }
->>>>>>> f4739e0f6ebce6bb37bce295fb5e1229a7632a98
-
-        RequestQueue queue = Volley.newRequestQueue(this);
-        queue.add(stringRequest);
     }
 }
--- Fuentes/app/src/main/java/cl/inacap/simpson/adapters/PersonajesAdapters.java
+++ Fuentes/app/src/main/java/cl/inacap/simpson/adapters/PersonajesAdapters.java
@@ -1,11 +1,8 @@
 package cl.inacap.simpson.adapters;
 
 import android.app.Activity;
-<<<<<<< HEAD
 import android.content.Context;
-=======
 import android.util.Log;
->>>>>>> f4739e0f6ebce6bb37bce295fb5e1229a7632a98
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,16 +10,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-<<<<<<< HEAD
-=======
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
->>>>>>> 264d5fc4d05ed4fb56e62ccf5cadf71859c6c330
-
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
@@ -47,15 +36,9 @@ import cl.inacap.simpson.dto.Personaje;
 
 public class PersonajesAdapters extends ArrayAdapter<Personaje>{
 
-<<<<<<< HEAD
     List<Personaje> personajes;
     Activity activity;
     Context context;
-=======
-
-    private List<Personaje> personajes;
-    private Activity activity;
->>>>>>> f4739e0f6ebce6bb37bce295fb5e1229a7632a98
 
     public PersonajesAdapters(@NonNull Context context, int resource, @NonNull List<Personaje> objects ) {
         super(context, resource, objects);
@@ -67,7 +50,7 @@ public class PersonajesAdapters extends ArrayAdapter<Personaje>{
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-<<<<<<< HEAD
+
         View view = convertView;
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view  = inflater.inflate(R.layout.list_personajes, null, true);
@@ -75,18 +58,6 @@ public class PersonajesAdapters extends ArrayAdapter<Personaje>{
         TextView nombre =    view.findViewById(R.id.nombre_txt);
         TextView frase =     view.findViewById(R.id.quote);
         ImageView img =      view.findViewById(R.id.image);
-=======
-<<<<<<< HEAD
-        TextView nombre = rV.findViewById(R.id.nombre_txt);
-        TextView frase = rV.findViewById(R.id.quote);
-        ImageView img = rV.findViewById(R.id.image);
-
-=======
-        TextView nombre =    rV.findViewById(R.id.nombre_txt);
-        TextView frase =     rV.findViewById(R.id.quote);
-        ImageView img =      rV.findViewById(R.id.image);
->>>>>>> 264d5fc4d05ed4fb56e62ccf5cadf71859c6c330
->>>>>>> f4739e0f6ebce6bb37bce295fb5e1229a7632a98
 
         nombre.setText(personajes.get(position).getCharacter());
         frase.setText(personajes.get(position).getQuote());
@@ -97,12 +68,9 @@ public class PersonajesAdapters extends ArrayAdapter<Personaje>{
         return view;
     }
 
-<<<<<<< HEAD
     @Override
     public int getCount() {
         return super.getCount();
     }
 
-=======
->>>>>>> f4739e0f6ebce6bb37bce295fb5e1229a7632a98
 }
